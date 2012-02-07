12ac5c6fd9378ef9e579b6121665342b2d7d6071
Robert Tully <sunstrike@azurenode.net>
2012-02-07 18:34:07 +0000

    Prepare foundations for test harness
    Signed-off-by: Robert Tully <sunstrike@azurenode.net>

---
+++ ASLibTestHarness/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ASLibTestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "0";
+            int doBreak = 0;
+
+            do
+            {
+                input = GetChoice();
+                if (input == "1" /*|| input == "2"*/) {
+                    doBreak = 1;
+                } else {
+                    Console.WriteLine("Invalid input.");
+                    Console.WriteLine();
+                    Thread.Sleep(1000);
+                }
+            } while (doBreak == 0);
+        }
+
+        private static string GetChoice()
+        {
+            Console.WriteLine("TEST HARNESSES:");
+            Console.WriteLine(" 1. ASComicAccess");
+            Console.WriteLine();
+            Console.Write("Enter option: ");
+            return Console.ReadLine();
+        }
+    }
+}
