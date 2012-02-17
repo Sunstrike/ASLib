5425d3452fd621f7ecceb08a8028cdc3082a01bd
Robert Tully <sunstrike@azurenode.net>
2012-02-17 18:06:16 +0000

    Minor change from Code Analysis, add basic edit functions and include compact/verify commands
    Signed-off-by: Robert Tully <sunstrike@azurenode.net>

--- ASLib/ASComicDatabase.cs
+++ ASLib/ASComicDatabase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Drawing;
 using System.Data.SqlServerCe;
 using System.Diagnostics;
 
@@ -74,13 +75,86 @@ namespace ASLib
                     catch (Exception ex)
                     {
                         Debug.WriteLine("EXCEPTION IN __initDatabase!\n" + ex.ToString()); // Log it...
-                        throw ex; // ...and fail.
+                        throw; // ...and fail.
                     }
-                    finally
-                    {
-                        conn.Close(); // Close connection (Possibly not required but it can't hurt)
-                    }
+                }
+            }
+        }
 
+        public bool verifyDatabase() // Calls SQLCE's verification function
+        {
+            string connString = @"Data Source = " + dbPath; // Create connection string for SQLCE DB
+            SqlCeEngine engine = new SqlCeEngine(connString); // Instantiate engine object
+
+            if (!engine.Verify())
+            {
+                throw new Exception("Database corrupt"); // Host application should use a try..catch block to catch this
+            }
+
+            return true; // Return true as Database is intact
+        }
+
+        public void compactDatabase() // Calls SQLCE's compactor
+        {
+            string connString = @"Data Source = " + dbPath; // Create connection string for SQLCE DB
+            SqlCeEngine engine = new SqlCeEngine(connString); // Instantiate engine object
+
+            // Do the compaction
+            engine.Compact(null);
+            // We then verify the database, just incase.
+            if (!engine.Verify())
+            {
+                throw new Exception("Database corrupt");
+            }
+        }
+
+        public void insertRow(int num, string img, string safe_title, string alt, string transcript, string date) // NOT including image DATA, as that's not 'compulsory' data
+        {
+            using (SqlCeConnection conn = new SqlCeConnection(@"Data Source = " + dbPath)) // DB connection
+            {
+                using (SqlCeCommand cmd = new SqlCeCommand(@"INSERT comics (num, img, safe_title, alt, transcript, date) VALUES (@num, @img, @safe_title, @alt, @transcript, @date)")) // SQL command
+                {
+                    // Add C# data to the SQL query
+                    cmd.Parameters.AddWithValue("@num", num);
+                    cmd.Parameters.AddWithValue("@img", img);
+                    cmd.Parameters.AddWithValue("@safe_title", safe_title);
+                    cmd.Parameters.AddWithValue("@alt", alt);
+                    cmd.Parameters.AddWithValue("@transcript", transcript);
+                    cmd.Parameters.AddWithValue("@date", date);
+
+                    conn.Open(); // Open DB connection
+                    cmd.ExecuteNonQuery(); // Run query
+                }
+            }
+        }
+
+        public void updateImgData(int num, Image image) // Used by Offline Mode to store actual images
+        {
+            using (SqlCeConnection conn = new SqlCeConnection(@"Data Source = " + dbPath)) // DB connection
+            {
+                using (SqlCeCommand cmd = new SqlCeCommand(@"UPDATE comics SET img_data = @img_data WHERE num = @num")) // SQL command
+                {
+                    // Add C# data to SQL query
+                    cmd.Parameters.AddWithValue("@num", num);
+                    cmd.Parameters.AddWithValue("@img_data", image);
+
+                    conn.Open(); // Open DB connection
+                    cmd.ExecuteNonQuery(); // Run query
+                }
+            }
+        }
+
+        public void deleteImgData(int num) // Used to purge Offline Mode images
+        {
+            using (SqlCeConnection conn = new SqlCeConnection(@"Data Source = " + dbPath)) // DB connection
+            {
+                using (SqlCeCommand cmd = new SqlCeCommand(@"UPDATE comics SET img_data = NULL WHERE num = @num")) // SQL command
+                {
+                    // Add C# data to SQL query
+                    cmd.Parameters.AddWithValue("@num", num);
+
+                    conn.Open(); // Open DB connection
+                    cmd.ExecuteNonQuery(); // Run query
                 }
             }
         }
