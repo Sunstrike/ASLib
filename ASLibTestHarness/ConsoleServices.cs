f67326689c3f77a03d572ff1aa64ac462c1a273e
Robert Tully <sunstrike@azurenode.net>
2012-03-03 14:59:26 +0000

    Add utility class to cleanup test harness
    Signed-off-by: Robert Tully <sunstrike@azurenode.net>

---
+++ ASLibTestHarness/ConsoleServices.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASLibTestHarness
+{
+    class ConsoleServices
+    {
+        public void writeFailText()
+        {
+            Console.Write("[");
+            writeRedText("FAIL");
+            Console.WriteLine("]");
+        }
+
+        public void writePassText()
+        {
+            Console.Write("[");
+            writeGreenText("PASS");
+            Console.WriteLine("]");
+        }
+
+        public void writeRedText(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed; // RED fail text
+            Console.Write(text);
+            Console.ResetColor(); // Back to normal colour
+        }
+
+        public void writeGreenText(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen; // GREEN pass text
+            Console.Write(text);
+            Console.ResetColor(); // Back to normal colour
+        }
+    }
+}
